6940041b076e0b44c77cb02415ac0b560e190ae3
GenericRicochetPlayer <113946528+GenericRicochetPlayer@users.noreply.github.com>
2024-08-02 18:01:33 -0400

    Add files via upload (#14)
    * Add files via upload
    
    * Update sh_move.lua

--- lua/weapons/cw_base/sh_move.lua
+++ lua/weapons/cw_base/sh_move.lua
@@ -6,33 +6,6 @@ local GetWalkSpeed = reg.Player.GetWalkSpeed
 local GetCrouchedWalkSpeed = reg.Player.GetCrouchedWalkSpeed
 local Crouching = reg.Player.Crouching
 
-local wep
-
-function CW_Move(ply, m)
-	if Crouching(ply) then
-		wep = GetActiveWeapon(ply)
-		
-		if IsValid(wep) and wep.CW20Weapon then
-			if wep.dt and wep.dt.State == CW_AIMING then
-				m:SetMaxSpeed((GetWalkSpeed(ply) * GetCrouchedWalkSpeed(ply) - wep.SpeedDec * 0.5))
-			end
-		end
-	else
-		wep = GetActiveWeapon(ply)
-		
-		if IsValid(wep) and wep.CW20Weapon then
-			if wep.dt and wep.dt.State == CW_AIMING then
-				m:SetMaxSpeed((GetWalkSpeed(ply) - wep.SpeedDec) * 0.75)
-			else
-				m:SetMaxSpeed(GetRunSpeed(ply) - wep.SpeedDec)
-			end
-		else
-			m:SetMaxSpeed(GetRunSpeed(ply))
-		end
-	end
-end
-
-hook.Add("Move", "CW_Move", CW_Move)
 
 function CW_StartCommand(ply, ucmd)
 	local wep = GetActiveWeapon(ply)
